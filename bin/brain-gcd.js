83f2135e635e1c78065e2db6536dbf267221e218
zaebbb <elik@example.com>
2024-07-22 15:17:47 +0500

    m

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -2,42 +2,45 @@
 
 import readlineSync from 'readline-sync';
 
-// Функция для нахождения наибольшего общего делителя (НОД)
 const gcd = (a, b) => {
-    while (b !== 0) {
-        let temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
 };
 
-const runBrainGcd = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-    console.log('Find the greatest common divisor of given numbers.');
-
-    // Цикл вопросов
-    let correct = true;
-    while (correct) {
-        const number1 = Math.floor(Math.random() * 100) + 1;
-        const number2 = Math.floor(Math.random() * 100) + 1;
-
-        console.log(`Question: ${number1} ${number2}`);
-        const userAnswer = readlineSync.question('Your answer: ');
-        const correctAnswer = gcd(number1, number2).toString();
-
-        if (userAnswer === correctAnswer) {
-            console.log('Correct!');
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Let's try again, ${name}!`);
-            correct = false;
-        }
-    }
-
-    console.log(`Congratulations, ${name}!`);
+// Функция для генерации случайного числа в заданном диапазоне [min, max]
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-runBrainGcd()
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Find the greatest common divisor of given numbers.');
+
+const roundsCount = 3; // количество раундов игры
+
+const playGame = () => {
+  for (let i = 0; i < roundsCount; i++) {
+    const number1 = generateRandomNumber(1, 100); // генерируем случайные числа от 1 до 100
+    const number2 = generateRandomNumber(1, 100);
+    const correctAnswer = gcd(number1, number2);
+
+    console.log(`Question: ${number1} ${number2}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (parseInt(userAnswer, 10) === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+playGame();
